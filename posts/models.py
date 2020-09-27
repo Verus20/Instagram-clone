e86d3ad4394cf67a68649ae9fcf43dcf1f7ed11f
Matt <matthewlevitt20@gmail.com>
2020-09-26 21:14:13 -0700

    another test

--- posts/models.py
+++ posts/models.py
@@ -33,7 +33,6 @@ class Post(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
-    integer = models.SmallIntegerField(default=0, blank=True, null=True)
 
     # def __str__(self):
     #     return self.content
